9632155f7726d9453f6a3b3a4dffa5166013bd58
Luan <luanjesus0@gmail.com>
2025-09-16 21:54:47 -0300

    subiu

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [showSplash, setShowSplash] = useState(false);
+  const [started, setStarted] = useState(false); // controla se o usuário clicou no botão
+ 
+  const handleStart = () => {
+    setStarted(true); // indica que o usuário clicou
+    setShowSplash(true); // mostra a splash
+   
+
+  const fadeOut = (audio, duration) => {
+    const stepTime = 50;
+    const steps = duration /  stepTime;
+    const stepVolume = audio.volume / steps;
+
+    const fadeInterval = setInterval(() => {
+
+      if (audio.volume - stepVolume > 0) {
+        audio.volume -= stepVolume;
+      }else{
+        audio.volume = 0;
+        audio.pause();
+        clearInterval(fadeInterval)
+      }
+
+    }, stepTime);
+  };
+
+
+    const audio = new Audio("./audio/lambo.mp3");
+    audio.volume= 1;
+    audio.play();
+    fadeOut(audio, 4000)
+
+
+    setTimeout(() => {
+      setShowSplash(false); // esconde a splash após 3 segundos
+    }, 60000);
+  };
+
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      {!started ? (
+        <button onClick={handleStart} className="start-button">
+          Acelere
+        </button>
+      ) : showSplash ? (
+        <div className="splash">
+         
+        </div>
+      ) : 
+        
+        <div className="conteudo">
+      
+         <div class="boxCarro" id="1"></div>
+         <div class="boxCarro" id="2"></div> 
+         <div class="boxCarro" id="3"></div>
+
+        </div>
+
+
+      }
     </div>
   );
 }
