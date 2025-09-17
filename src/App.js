bbdff3f10d0e14a0bb5fc6985b292f31ab1212f8
unknown <LUAN_23063@lab.etec187>
2025-09-17 12:20:23 -0300

    novo

--- src/App.js
+++ src/App.js
@@ -4,63 +4,67 @@ import { useEffect, useState } from 'react';
 
 function App() {
   const [showSplash, setShowSplash] = useState(false);
-  const [started, setStarted] = useState(false); // controla se o usuário clicou no botão
- 
+  const [started, setStarted] = useState(false);
+  const [appBranco, setAppBranco] = useState(false); // controla se o usuário clicou no botão
+
   const handleStart = () => {
     setStarted(true); // indica que o usuário clicou
     setShowSplash(true); // mostra a splash
-   
 
-  const fadeOut = (audio, duration) => {
-    const stepTime = 50;
-    const steps = duration /  stepTime;
-    const stepVolume = audio.volume / steps;
 
-    const fadeInterval = setInterval(() => {
+    const fadeOut = (audio, duration) => {
+      const stepTime = 50;
+      const steps = duration / stepTime;
+      const stepVolume = audio.volume / steps;
 
-      if (audio.volume - stepVolume > 0) {
-        audio.volume -= stepVolume;
-      }else{
-        audio.volume = 0;
-        audio.pause();
-        clearInterval(fadeInterval)
-      }
+      const fadeInterval = setInterval(() => {
 
-    }, stepTime);
-  };
+        if (audio.volume - stepVolume > 0) {
+          audio.volume -= stepVolume;
+        } else {
+          audio.volume = 0;
+          audio.pause();
+          clearInterval(fadeInterval)
+        }
+
+      }, stepTime);
+    };
 
 
     const audio = new Audio("./audio/lambo.mp3");
-    audio.volume= 1;
+    audio.volume = 1;
     audio.play();
     fadeOut(audio, 4000)
 
-
+    setTimeout(() => {
+      setAppBranco(true)
+    }, 1000);
     setTimeout(() => {
       setShowSplash(false); // esconde a splash após 3 segundos
-    }, 60000);
+    }, 5000);
   };
 
-  
-  
+
+
   return (
-    <div className="App">
-      
+    <div className={!appBranco ? "App" : "App2"}>
+
       {!started ? (
-        <button onClick={handleStart} className="start-button">
-          Acelere
-        </button>
+        
+          <button onClick={handleStart} className="start-button">
+            Acelere
+          </button>
       ) : showSplash ? (
         <div className="splash">
-         
+
         </div>
-      ) : 
-        
+      ) :
+
         <div className="conteudo">
-      
-         <div class="boxCarro" id="1"></div>
-         <div class="boxCarro" id="2"></div> 
-         <div class="boxCarro" id="3"></div>
+
+          <div className="boxCarro" id="um"></div>
+          <div className="boxCarro" id="dois"></div>
+          <div className="boxCarro" id="tres"></div>
 
         </div>
 
